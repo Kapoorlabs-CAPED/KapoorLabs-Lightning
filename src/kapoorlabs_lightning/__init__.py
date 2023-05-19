3d05c82d5f84a4335348add37ad3d4d35341f797
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-05-19 13:09:00 +0200

    cellshape datasets

--- src/kapoorlabs_lightning/__init__.py
+++ src/kapoorlabs_lightning/__init__.py
@@ -10,21 +10,29 @@ from .lightning_trainer import (
     LightningModel,
     LightningTrain,
 )
-from .pytorch_datasets import MiniEcoset, MNISTDataModule, TFIODataset
+from .pytorch_datasets import (
+    PointCloudDataset,
+    PointCloudNpzDataset,
+    PyntCloud,
+    ShapeNetDataset,
+    SingleCellDataset,
+)
 from .pytorch_loggers import CustomNPZLogger
 from .pytorch_models import __all__ as all_pytorch_models
 from .pytorch_transforms import Transforms
 
 __all__ = [
-    "MiniEcoset",
-    "TFIODataset",
-    "MNISTDataModule",
-    "LightningModel",
     "AutoLightningModel",
-    "LightningTrain",
     "AutoLightningTrain",
-    "LightningData",
-    "Transforms",
     "CustomNPZLogger",
+    "LightningData",
+    "LightningModel",
+    "LightningTrain",
+    "PointCloudDataset",
+    "PointCloudNpzDataset",
+    "PyntCloud",
+    "ShapeNetDataset",
+    "SingleCellDataset",
+    "Transforms",
 ]
 __all__.extend(all_pytorch_models)
