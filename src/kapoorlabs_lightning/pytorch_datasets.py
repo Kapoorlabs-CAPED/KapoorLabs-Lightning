531333ab7920fdd2e8a76b123c0e4d58201d2447
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-05-25 17:03:30 +0000

    print

--- src/kapoorlabs_lightning/pytorch_datasets.py
+++ src/kapoorlabs_lightning/pytorch_datasets.py
@@ -145,7 +145,7 @@ class MitosisDataset(Dataset):
         array = self.arrays[idx]
         array = torch.tensor(array).permute(1, 0).float()
         label = torch.tensor(self.labels[idx])
-        print(array.shape, label.shape)
+        print(array.shape, label)
         return array, label
 
 
