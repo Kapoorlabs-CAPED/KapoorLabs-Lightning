c962fc93173d0c281f1c1c942b7ac9fa1d5f64e6
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-08-04 13:22:46 +0000

    up

--- src/kapoorlabs_lightning/pytorch_datasets.py
+++ src/kapoorlabs_lightning/pytorch_datasets.py
@@ -159,7 +159,7 @@ class H5MitosisDataset(Dataset):
 
             label = torch.from_numpy(np.asarray(self.targets[idx]))
 
-            
+            print(array.shape)
             return array, label
 
 
