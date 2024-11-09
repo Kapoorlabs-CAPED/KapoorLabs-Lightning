20e0120896bc969beec50c76a441a0823a4ba991
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-09 00:53:13 +0000

    cp

--- src/kapoorlabs_lightning/pytorch_losses.py
+++ src/kapoorlabs_lightning/pytorch_losses.py
@@ -116,8 +116,9 @@ class VolumeYoloLoss(nn.Module):
 
     def forward(self, y_true, y_pred):
 
-        y_true = y_true.squeeze(-1).squeeze(-1).squeeze(-1)
-
+        
+        y_true = y_true.reshape(y_pred.shape)
+        print(y_true.shape, y_pred.shape)
         y_true = y_true.to(self.device)
         y_pred = y_pred.to(self.device)
         
