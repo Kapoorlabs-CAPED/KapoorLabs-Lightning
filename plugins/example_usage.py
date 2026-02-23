a323c735ec5bbf3ed53ee292edce7798215bbe46
kapoorlab <varun.kapoor@kapoorlabs.org>
2026-02-23 15:33:01 +0000

    change handler

--- plugins/example_usage.py
+++ plugins/example_usage.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
     # Create napari viewer
     viewer = napari.Viewer()
 
-    # Add the ONEAT visualizer widget
-    widget = oneat_visualizer_widget()
-    viewer.window.add_dock_widget(widget, name="ONEAT Events")
+    # Add the ONEAT visualizer widget (napari will inject the viewer automatically)
+    viewer.window.add_dock_widget(oneat_visualizer_widget, name="ONEAT Events")
 
     # Start napari
     napari.run()
