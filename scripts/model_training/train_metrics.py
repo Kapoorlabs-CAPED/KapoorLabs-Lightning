a0f632b8235c56b61b3025dc935059724bbe7b5f
kapoorlab <varun.kapoor@kapoorlabs.org>
2026-03-01 16:18:02 +0000

    tli

---
+++ scripts/model_training/train_metrics.py
@@ -0,0 +1,23 @@
+# %%
+from pathlib import Path
+from kapoorlabs_lightning.utils import  plot_npz_files_interactive
+import os
+
+
+# %%
+npz_directory_current=Path('/projects/extern/nhr/nhr_ni/nhr_ni_test/nhr_ni_test_27040/dir.project/oneat_mitosis_model/')
+npz_directory_backup=Path('/projects/extern/nhr/nhr_ni/nhr_ni_test/nhr_ni_test_27040/dir.project/oneat_mitosis_model/backup/')
+
+npz_file_current = [npz_directory_current/file for file in os.listdir(npz_directory_current) if file.endswith('.npz')][-1]
+#npz_file_backup = [npz_directory_backup/file for file in os.listdir(npz_directory_backup) if file.endswith('.npz')]
+
+filepaths =  [npz_file_current] #+  [npz_file for npz_file in npz_file_backup] 
+
+# %%
+
+plot_npz_files_interactive(filepaths, save_plots=True, page_output_dir=npz_directory_current.stem)
+
+# %%
+
+
+
