ef5c26cd7d4367c95bea03ca0943e21af430d439
kapoorlab <randomaccessiblekapoor@gmail.com>
2026-04-05 12:27:27 +0200

    update jean zay

---
+++ scripts/model_training/slurm_train_cellfate_jean_zay.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1             # Number of nodes 
+#SBATCH -A lzc@a100
+#SBATCH -C a100
+#SBATCH --gres=gpu:1       # Allocate 4 GPUs per node
+#SBATCH --partition=gpu_p5
+#SBATCH --job-name=care_train               # Jobname 
+#SBATCH --cpus-per-task=40
+#SBATCH --output=cell.o%j            # Output file 
+#SBATCH --error=cell.o%j            # Error file 
+#SBATCH --time=20:00:00       # Expected runtime HH:MM:SS (max 100h)
+module purge # purging modules inherited by default
+
+module load anaconda-py3/2020.11
+#conda init bash # deactivating environments inherited by default
+conda deactivate
+conda activate capedenv
+
+module load cuda/11.8.0
+
+# Run training with combined dataset
+# Uses --config-name to override default scenario with combined config
+srun --unbuffered python lightning-cellfate.py
